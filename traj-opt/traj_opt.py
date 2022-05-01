38f289ecaeeae3953c468ad6d43adeaaa09ad278
James Arnold <arnoldjames98@gmail.com>
2022-05-01 17:37:17 -0400

    Working traj opt with time varying LQR pipeline

--- traj-opt/traj_opt.py
+++ traj-opt/traj_opt.py
@@ -48,19 +48,36 @@ from pydrake.all import (
 from pydrake.examples.acrobot import AcrobotGeometry, AcrobotPlant
 from pydrake.examples.pendulum import PendulumPlant, PendulumState
 
+from pydrake.systems.framework import BasicVector, LeafSystem, PortDataType
+
 from underactuated import FindResource, running_as_notebook
 from underactuated.jupyter import AdvanceToAndVisualize
 from underactuated.meshcat_utils import draw_points, set_planar_viewpoint
 from underactuated.pendulum import PendulumVisualizer
 
 # %%
-# Start Meshcat
+# Start Meshcat and load model
 # Start the visualizer (run this cell only once, each instance consumes a port)
 meshcat = StartMeshcat()
 
+# Triple cart pole model
+TRIPLE_CARTPOLE_URDF = "../triple_cartpole.urdf"
+
 # %%
 # Solve trajectory optimization
-NUM_BREAKPOINTS = 21
+
+# NUM_BREAKPOINTS = 21
+# MIN_TIMESTEP = 0.1
+# MAX_TIMESTEP = 0.4
+# NOTE: One suprising thing is how different the force profiles are when these values are changed even slightly
+# NUM_BREAKPOINTS = 81
+# MIN_TIMESTEP = 0.025
+# MAX_TIMESTEP = 0.1
+
+NUM_BREAKPOINTS = 161
+MIN_TIMESTEP = 0.00175
+MAX_TIMESTEP = 0.025
+
 SIMULATION_TIMESTEP = 0.01
 FLAT_SIMULATION = False
 MAX_SIMULATION_TIME = 6  # seconds after which to stop meshcat simulation
@@ -68,8 +85,7 @@ MAX_SIMULATION_TIME = 6  # seconds after which to stop meshcat simulation
 plant = MultibodyPlant(time_step=0.0)
 scene_graph = SceneGraph()
 plant.RegisterAsSourceForSceneGraph(scene_graph)
-# file_name = FindResource("models/cartpole.urdf")
-Parser(plant).AddModelFromFile("triple_cartpole.urdf")
+Parser(plant).AddModelFromFile(TRIPLE_CARTPOLE_URDF)
 plant.Finalize()
 
 context = plant.CreateDefaultContext()
@@ -77,8 +93,8 @@ dircol = DirectCollocation(
     plant,
     context,
     num_time_samples=NUM_BREAKPOINTS,
-    minimum_timestep=0.1,
-    maximum_timestep=0.4,
+    minimum_timestep=MIN_TIMESTEP,
+    maximum_timestep=MAX_TIMESTEP,
     input_port_index=plant.get_actuation_input_port().get_index(),
 )
 prog = dircol.prog()
@@ -94,8 +110,8 @@ final_state = (0.0, np.pi, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 prog.AddBoundingBoxConstraint(final_state, final_state, dircol.final_state())
 # prog.AddLinearConstraint(dircol.final_state() == final_state)
 
-
-R = 10  # Cost on input "effort".
+# R = 10
+R = 1000  # Cost on input "effort".
 u = dircol.input()
 dircol.AddRunningCost(R * u[0] ** 2)
 
@@ -138,7 +154,7 @@ display(plt.show())
 x_trajectory = dircol.ReconstructStateTrajectory(result)
 
 # %%
-# Prepare the simulation
+# Prepare the simulation of trajectory optimization
 
 builder = DiagramBuilder()
 source = builder.AddSystem(TrajectorySource(x_trajectory))
@@ -165,7 +181,8 @@ simulator = Simulator(diagram)
 simulator.set_publish_every_time_step(False)  # makes sim faster
 
 context = simulator.get_mutable_context()
-# %% Run the simulation
+# %%
+# % Run the simulation of trajectory optimization
 context.SetTime(0)
 
 # run simulation
@@ -212,21 +229,12 @@ while meshcat.GetButtonClicks("Stop Simulation") < 1:
 
 meshcat.DeleteAddedControls()
 
-
-# %% Show drake block diagram
-display(
-    SVG(
-        pydot.graph_from_dot_data(diagram.GetGraphvizString(max_depth=2))[
-            0
-        ].create_svg()
-    )
-)
 # %%
-# Run open loop simulation
+# Run open loop simulation of trajectory optimization result
 builder = DiagramBuilder()
 
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
-Parser(plant).AddModelFromFile("triple_cartpole.urdf")
+Parser(plant).AddModelFromFile(TRIPLE_CARTPOLE_URDF)
 plant.Finalize()
 
 visualizer = MeshcatVisualizerCpp.AddToBuilder(builder, scene_graph, meshcat)
@@ -267,4 +275,134 @@ state = log.data()
 
 ax.plot(log.sample_times(), state[0], label=f"actual")
 ax.legend()
+plt.show()
+
+
+# %%
+# Finite Horizon LQR to stabilze trajectory
+
+builder = DiagramBuilder()
+
+plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
+Parser(plant).AddModelFromFile(TRIPLE_CARTPOLE_URDF)
+# plant.DeclareInputPort("input", PortDataType.kVectorValued, 1)
+plant.Finalize()
+
+visualizer = MeshcatVisualizerCpp.AddToBuilder(builder, scene_graph, meshcat)
+logger = LogVectorOutput(plant.get_state_output_port(), builder)
+meshcat.Delete()
+# meshcat.Set2dRenderMode(xmin=-4, xmax=1, ymin=-1, ymax=1)
+
+Q = np.diag([10.0, 10.0, 10.0, 10.0, 1.0, 1.0, 1.0, 1.0])
+# NOTE: Tried reducing R so that there is not a great cost on input
+R = 0.001 * np.eye(1)
+options = FiniteHorizonLinearQuadraticRegulatorOptions()
+# NOTE: Tried removing this final cost on the state
+# NOTE: Tried making these values much bigger to penalize not reaching final state
+options.Qf = np.diag([1000.0, 1000.0, 1000.0, 1000.0, 1.0, 1.0, 1.0, 1.0])
+# options.use_square_root_method = True  # Pending drake PR #16812
+options.x0 = x_trajectory
+options.u0 = u_trajectory
+options.input_port_index = plant.get_actuation_input_port().get_index()
+
+# NOTE: The following line takes a long time
+controller = builder.AddSystem(
+    MakeFiniteHorizonLinearQuadraticRegulator(
+        system=plant,
+        context=plant.CreateDefaultContext(),
+        t0=x_trajectory.start_time(),
+        tf=x_trajectory.end_time(),
+        Q=Q,
+        R=R,
+        options=options,
+    )
+)
+builder.Connect(controller.get_output_port(), plant.get_actuation_input_port())
+builder.Connect(plant.get_state_output_port(), controller.get_input_port())
+
+diagram = builder.Build()
+
+simulator = Simulator(diagram)
+context = simulator.get_mutable_context()
+plant_context = plant.GetMyContextFromRoot(context)
+
+ts = np.linspace(u_trajectory.start_time(), u_trajectory.end_time(), 301)
+# We plot this later on
+desired_state = x_trajectory.vector_values(ts)
+
+# %%
+
+# Simulation the time varying LQR response (not recorded)
+# This is a version that you can step through in the debugger
+# The next cell advances to the end and records it
+meshcat.AddButton("Stop Simulation")
+rng = np.random.default_rng(123)
+
+# for i in range(1):
+context.SetTime(x_trajectory.start_time())
+# initial_state = GliderState(x_traj.value(x_traj.start_time()))
+initial_state = x_trajectory.value(x_trajectory.start_time())
+# TODO: Add random noise to initial condition
+# initial_state[0] += 0.04*rng.standard_normal()
+plant_context.SetContinuousState(initial_state[:])
+
+simulator.Initialize()
+simulator.set_target_realtime_rate(1.0)
+
+# state_traj = []
+input("Press Enter to start simulation after 2 seconds")
+time.sleep(2)  # Give time to switch to meshcat window
+
+while meshcat.GetButtonClicks("Stop Simulation") < 1:
+    print("Time:", simulator.get_context().get_time())
+    # x = simulator.get_context().get_continuous_state().get_generalized_position().GetAtIndex(0)
+
+    state = simulator.get_context().get_continuous_state().get_vector().CopyToVector()
+    # print(state)
+    # state_traj.append(state)
+    if simulator.get_context().get_time() >= 6:
+        break
+    # Advance the simulation forward
+    simulator.AdvanceTo(simulator.get_context().get_time() + SIMULATION_TIMESTEP)
+    # Make the meshcat animation match
+    time.sleep(SIMULATION_TIMESTEP)
+
+meshcat.DeleteAddedControls()
+
+# %%
+# Simulation the time varying LQR response
+rng = np.random.default_rng(123)
+
+# for i in range(1):
+context.SetTime(x_trajectory.start_time())
+# initial_state = GliderState(x_traj.value(x_traj.start_time()))
+initial_state = x_trajectory.value(x_trajectory.start_time())
+# TODO: Add random noise to initial condition
+# initial_state[0] += 0.04*rng.standard_normal()
+plant_context.SetContinuousState(initial_state[:])
+
+input("Press Enter to start simulation after 2 seconds")
+time.sleep(2)  # Give time to switch to meshcat window
+
+simulator.Initialize()
+simulator.AdvanceTo(x_trajectory.end_time())
+# log.Clear()
+visualizer.PublishRecording()
+log = logger.FindLog(context)
+state = log.data()
+
+# %%
+# Plot the results of the time varying LQR
+
+fig, ax = plt.subplots(figsize=(14, 6))
+ax.plot(ts, desired_state[0], label="desired")
+ax.set_xlabel("time")
+ax.set_ylabel("cart position")
+
+ax.plot(log.sample_times(), state[0], "r--", label=f"actual")
+
+ax.legend()
+# In case the result blows up, at least see how close it was
+# ax.set_ylim([-1, 0.5])
+plt.show()
 # %%
